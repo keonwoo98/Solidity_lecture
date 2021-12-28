fcc2a9de20e29d28886e863ad151290a52ffe9d8
keonwoo98 <keonwoo98@naver.com>
2021-12-28 18:15:11 +0900

    1강: 매우 간단한 smart contract 생성

--- solidity01.sol
+++ solidity01.sol
@@ -2,6 +2,6 @@
 
 pragma solidity >= 0.7.0 < 0.9.0;		// 솔리디티의 컴파일 버전 명시 (0.7~0.9의 버전 사용)
 
-contract hello{			// 스마트 컨트랙트 명시
+contract hello {			// 스마트 컨트랙트 명시
 	string public hi = "Hello Solidity";		// hi라는 public 함수에 Hello Solidity 넣기
 }			// 매우 간단한 스마트 컨트랙트 완성
